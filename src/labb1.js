08074c482192399769102c2bba4a4cd783876389
Maddelen Hedenstrom <mh222vu@student.lnu.se>
2023-08-30 09:52:46 +0200

    Add array of greetings

---
+++ src/labb1.js
@@ -0,0 +1,26 @@
+const greetings = [
+  "Hello,", 
+  "Hi there,", 
+  "Greetings,", 
+  "Hey, you look lovely,", 
+  "Nice to meet you,"
+]
+
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.getElementById('button')
+  const nameInput = document.getElementById('nameInput')
+  const message = document.getElementById('message')
+
+  button.addEventListener('click', function () {
+    const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)]
+    const name = nameInput.value
+    
+    if (name) {
+      message.textContent = `${randomGreeting} ${name}!`
+      nameInput.value = ''
+      
+    } else {
+      message.textContent = 'Please enter your name.'
+    }
+  })
+})
