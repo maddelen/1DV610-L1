6beaddbaa7a90d4428b7ad724399f46cb7c73451
Maddelen Hedenstrom <mh222vu@student.lnu.se>
2023-08-30 10:06:35 +0200

    Eslint fix

--- src/labb1.js
+++ src/labb1.js
@@ -1,9 +1,9 @@
 const greetings = [
-  "Hello,", 
-  "Hi there,", 
-  "Greetings,", 
-  "Hey, you look lovely,", 
-  "Nice to meet you,"
+  'Hello,',
+  'Hi there,',
+  'Greetings,',
+  'Hey, you look lovely,',
+  'Nice to meet you,'
 ]
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -14,11 +14,10 @@ document.addEventListener('DOMContentLoaded', function () {
   button.addEventListener('click', function () {
     const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)]
     const name = nameInput.value
-    
+
     if (name) {
       message.textContent = `${randomGreeting} ${name}!`
       nameInput.value = ''
-      
     } else {
       message.textContent = 'Please enter your name.'
     }
